669437388dfd9c55fccb5bf3ecaf515898427ade
Guido Mintjes <guidomintjes@gmail.com>
2021-10-01 15:53:57 +0200

    Add project files.

---
+++ TCPChatClient/TCP.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPChatClient {
+    public class TCP {
+
+        
+        public TcpClient socket;    // Information stored that gets saved in server in the callback method
+        private readonly int id;
+
+        private NetworkStream stream;
+        private byte[] receiveByteArray;
+
+        public static int dataBufferSize = 4096;
+
+        public TCP(int _id) {
+
+            id = _id;
+        }
+
+
+        public void Connect(string ip, int port) {
+
+
+            socket = new TcpClient {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+
+            receiveByteArray = new byte[dataBufferSize];    // Gets the 'stream' of info provided by the socket
+            
+            socket.BeginConnect(ip, port, SocketConnectCallback, socket);
+        }
+
+
+        // Method that gets called back on when client connects to server
+        void SocketConnectCallback(IAsyncResult aResult) {
+
+            socket.EndConnect(aResult);
+
+            if(!socket.Connected) {
+                return;
+            }
+
+            stream = socket.GetStream();
+            stream.BeginRead(receiveByteArray, 0, dataBufferSize, StreamReceiveCallback, null);
+
+            Console.WriteLine("Connected to server on port: " + TCPChatClient.defaultPort);
+        }
+
+
+        void StreamReceiveCallback(IAsyncResult aResult) {
+
+            try {
+                socket.EndConnect(aResult);
+
+                if (!socket.Connected) {
+                    return;
+                }
+
+                stream = socket.GetStream();
+                stream.BeginRead(receiveByteArray, 0, dataBufferSize, StreamReceiveCallback, null);
+            } catch {
+
+                Console.WriteLine("Error! ==> disconnecting");
+            }
+        }
+    }
+}
---
+++ TCPChatClient/TCPChatClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace TCPChatClient {
+
+    public class TCPChatClient {
+
+
+        public static string defaultIP = "145.107.110.146";
+        public static int defaultPort = 8900;
+        public static int defaultID = 1;
+
+        public static TCP tcp;
+
+        public static void Main() {
+
+            Console.Title = "TCP Chat Client Demo";
+
+            tcp = new TCP(defaultID);
+
+            Console.WriteLine("Trying to connect to server on port: " + defaultPort);
+            tcp.Connect(defaultIP, defaultPort);
+
+            Console.Read();
+        }
+    }
+}
